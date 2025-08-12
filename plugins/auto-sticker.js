59a586863cf5c56424c8f37d63e87f45a96a94f4
Pkdriller <nexuspk04@gmail.com>
2025-08-12 18:36:29 +0300

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -1,32 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('../config');
-const { cmd } = require('../command');
+const config = require('../config')
+const {cmd , commands} = require('../command')
 
+//auto reply 
 cmd({
   on: "body"
-},
-async (conn, mek, m, { from, body }) => {
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
     const filePath = path.join(__dirname, '../assets/autosticker.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
+            
             if (config.AUTO_STICKER === 'true') {
-                const stickerPath = path.join(__dirname, '../assets/autosticker.json', data[text]);
-
-                if (fs.existsSync(stickerPath)) {
-                    const stickerBuffer = fs.readFileSync(stickerPath);
-
-                    await conn.sendMessage(from, {
-                        sticker: stickerBuffer,
-                        packname: 'PK-XMD',
-                        author: 'AUTO-STICKER'
-                    }, { quoted: mek });
-                } else {
-                    console.warn(`Sticker not found: ${stickerPath}`);
-                }
+                //if (isOwner) return;        
+                await m.reply(data[text])
+            
             }
         }
-    }
+    }                
 });
